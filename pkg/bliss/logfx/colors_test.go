1a89d3ac2cdcc70acfc2530f45f00530ff52ffd1
Sami Salih İbrahimbaş <contact@9ssi7.dev>
2024-08-19 19:11:46 +0300

    feat/testing: add logfx module tests with full coverage

---
+++ pkg/bliss/logfx/colors_test.go
@@ -0,0 +1,48 @@
+package logfx_test
+
+import (
+	"testing"
+
+	"github.com/eser/go-service/pkg/bliss/logfx"
+)
+
+func TestColored_ColorReset(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name     string
+		color    logfx.Color
+		expected string
+	}{
+		{
+			name:     "ColorReset",
+			color:    logfx.ColorReset,
+			expected: "\033[0m",
+		},
+		{
+			name:     "ColorRed",
+			color:    logfx.ColorRed,
+			expected: "\033[31m",
+		},
+		{
+			name:     "ColorGreen",
+			color:    logfx.ColorGreen,
+			expected: "\033[32m",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := logfx.Colored(tt.color, "test message")
+			expect := string(tt.color) + "test message" + string(logfx.ColorReset)
+			if result != expect {
+				t.Errorf("Colored() = %v, want %v", result, tt.expected)
+			}
+		})
+	}
+}
+
+func BenchmarkColored(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		logfx.Colored(logfx.ColorGreen, "test message")
+	}
+}
---
+++ pkg/bliss/logfx/replacer_test.go
@@ -0,0 +1,178 @@
+package logfx
+
+import (
+	"errors"
+	"fmt"
+	"runtime"
+	"strings"
+	"testing"
+
+	"log/slog"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type mockErr struct {
+	msg   string
+	stack []uintptr
+}
+
+func (m *mockErr) StackTrace() []uintptr {
+	return m.stack
+}
+
+func (m *mockErr) Error() string {
+	return m.msg
+}
+
+func (m *mockErr) Add(ptr uintptr) *mockErr {
+	m.stack = append(m.stack, ptr)
+	return m
+}
+
+func TestReplacerGenerator(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name       string
+		prettyMode bool
+		groups     []string
+		attr       slog.Attr
+		expected   slog.Attr
+	}{
+		{
+			name:       "PrettyMode=true, Key=slog.TimeKey",
+			prettyMode: true,
+			groups:     []string{},
+			attr:       slog.Attr{Key: slog.TimeKey},
+			expected:   slog.Attr{},
+		},
+		{
+			name:       "PrettyMode=true, Key=slog.LevelKey",
+			prettyMode: true,
+			groups:     []string{},
+			attr:       slog.Attr{Key: slog.LevelKey},
+			expected:   slog.Attr{},
+		},
+		{
+			name:       "PrettyMode=true, Key=slog.MessageKey",
+			prettyMode: true,
+			groups:     []string{},
+			attr:       slog.Attr{Key: slog.MessageKey},
+			expected:   slog.Attr{},
+		},
+		{
+			name:       "PrettyMode=false, Key=slog.TimeKey",
+			prettyMode: false,
+			groups:     []string{},
+			attr:       slog.Attr{Key: slog.TimeKey},
+			expected:   slog.Attr{Key: slog.TimeKey},
+		},
+		{
+			name:       "PrettyMode=false, Key=slog.LevelKey",
+			prettyMode: false,
+			groups:     []string{},
+			attr:       slog.Attr{Key: slog.LevelKey},
+			expected:   slog.Attr{Key: slog.LevelKey},
+		},
+		{
+			name:       "PrettyMode=false, Key=slog.MessageKey",
+			prettyMode: false,
+			groups:     []string{},
+			attr:       slog.Attr{Key: slog.MessageKey},
+			expected:   slog.Attr{Key: slog.MessageKey},
+		},
+		{
+			name:       "PrettyMode=false, Key=slog.TimeKey, Value=error",
+			prettyMode: false,
+			groups:     []string{},
+			attr:       slog.Attr{Key: slog.TimeKey, Value: slog.AnyValue(errors.New("test error"))},
+			expected:   slog.Attr{Key: slog.TimeKey, Value: slog.GroupValue(slog.String("msg", "test error"))},
+		},
+		{
+			name:       "PrettyMode=false, Key=slog.TimeKey, Value=error with StackTracer",
+			prettyMode: false,
+			groups:     []string{},
+			attr:       slog.Attr{Key: slog.TimeKey, Value: slog.AnyValue(errors.New("test error"))},
+			expected:   slog.Attr{Key: slog.TimeKey, Value: slog.GroupValue(slog.String("msg", "test error"))},
+		},
+		{
+			name:       "PrettyMode=false, Key=slog.TimeKey, Value=error with mockErr",
+			prettyMode: false,
+			groups:     []string{},
+			attr:       slog.Attr{Key: slog.TimeKey, Value: slog.AnyValue(&mockErr{msg: "test error"})},
+			expected:   slog.Attr{Key: slog.TimeKey, Value: slog.GroupValue(slog.String("msg", "test error"), slog.Any("trace", []string{}))},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := replacerGenerator(tt.prettyMode)(tt.groups, tt.attr)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+func TestTraceLines(t *testing.T) {
+	t.Parallel()
+	stackGenerator := func() []uintptr {
+		var pc [32]uintptr
+		n := runtime.Callers(0, pc[:])
+		return pc[:n]
+	}
+
+	stackGeneratorWithUnkownFunctionAddr := func() []uintptr {
+		var pc [32]uintptr
+		n := runtime.Callers(0, pc[:])
+		pc[0] = 0
+		return pc[:n]
+	}
+
+	pwd := func() string {
+		_, file, _, _ := runtime.Caller(0)
+		return file
+	}
+
+	tests := []struct {
+		name     string
+		stack    []uintptr
+		expected []string
+	}{
+		{
+			name:     "Empty Stack",
+			stack:    []uintptr{},
+			expected: []string{},
+		},
+		{
+			name:  "Non-Empty Stack",
+			stack: stackGenerator(),
+			expected: []string{
+				"runtime.Callers /usr/local/go/src/runtime/extern.go:331",
+				fmt.Sprint("github.com/eser/go-service/pkg/bliss/logfx.TestTraceLines.func1 ", pwd(), ":117"),
+				fmt.Sprint("github.com/eser/go-service/pkg/bliss/logfx.TestTraceLines ", pwd(), ":145"),
+				"testing.tRunner /usr/local/go/src/testing/testing.go:1690",
+			},
+		},
+		{
+			name:  "Non-Empty Stack with Unknown Function Address",
+			stack: stackGeneratorWithUnkownFunctionAddr(),
+			expected: []string{
+				"unknown",
+				fmt.Sprint("github.com/eser/go-service/pkg/bliss/logfx.TestTraceLines ", pwd(), ":147"),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := traceLines(tt.stack)
+			if len(tt.expected) == 0 {
+				assert.Equal(t, tt.expected, result)
+			} else {
+				for i := range tt.expected {
+					ext := strings.Split(tt.expected[i], " ")
+					assert.Contains(t, result[i], ext[0])
+				}
+			}
+		})
+	}
+}
